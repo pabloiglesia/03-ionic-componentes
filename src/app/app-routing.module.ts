53b7a6aeb84561836fc80eaf061765216ba9f5ba
Pablo Iglesia <pabloiglesia@outlook.com>
2022-11-07 20:38:54 +0100

    34. Navegación entre páginas
    - Se crea página "alert" (ionic g page pages/alert --spec=false)
    - Se crea página "action-sheet" (ionic g page pages/action-sheet --spec=false)
    - Se crean 2 botones para navegar desde inicio a estas páginas con  <ion-button routerLink="/alert">Alert</ion-button>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   
   {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full'
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
