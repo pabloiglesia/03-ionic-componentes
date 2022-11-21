c7cdfd04617fa8f1bea2cc5e85973ad5d7903b18
Pablo Iglesia <pabloiglesia@outlook.com>
2022-11-21 20:12:25 +0100

    44. ion-alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  handlerMessage = ''
+  roleMessage = ''
+
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled'
+            console.log(this.handlerMessage)
+          },
+        },
+        {
+          text: 'OK',
+          role: 'destructive',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed'
+            console.log(this.handlerMessage)
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertForm() {
+    const alert = await this.alertCtrl.create({
+      header: 'Please enter your info',
+      buttons: [
+        {
+
+          text: 'OK',
+          handler: ( data ) => {
+            console.log( data )
+          }
+        }
+      ],
+      inputs: [
+        {
+          name: 'name',
+          id: 'name',
+          placeholder: 'Name',
+        },
+        {
+          name: 'nickname',
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          }
+        },
+        {
+          name: 'age',
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          name: 'description',
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
