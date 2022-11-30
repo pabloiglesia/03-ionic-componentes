fca25982d44d664ba06cbd1a6ceb9824697f2026
Pablo Iglesia <pabloiglesia@outlook.com>
2022-11-30 19:08:58 +0100

    ion-grid (https://ionicframework.com/docs/api/grid)

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -56,6 +56,11 @@ export class InicioPage implements OnInit {
       name: 'Fab',
       redirectTo: '/fab'
     },
+    {
+      icon: 'grid-outline',
+      name: 'Grid',
+      redirectTo: '/grid'
+    },
 
 
   ]
