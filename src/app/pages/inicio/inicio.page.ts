ec33e91d6d69053b48a1ab59c3770262725e47ce
Pablo Iglesia <pabloiglesia@outlook.com>
2022-12-07 19:40:16 +0100

    ion-list (https://ionicframework.com/docs/api/list)
    Nos conectamos con https://jsonplaceholder.typicode.com/ : https://jsonplaceholder.typicode.com/users
    
    Creamos el servicio con ionic g service services/data y borramos el archivo de pruebas

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -71,6 +71,11 @@ export class InicioPage implements OnInit {
       name: 'Input Forms',
       redirectTo: '/input'
     },
+    {
+      icon: 'list-outline',
+      name: 'List - Sliding',
+      redirectTo: '/list'
+    },
 
 
   ]
