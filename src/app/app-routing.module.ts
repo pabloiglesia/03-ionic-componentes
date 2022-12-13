b3d4bfe61633c6316c1234f8b5ba12d10e114ce8
Pablo Iglesia <pabloiglesia@outlook.com>
2022-12-13 16:10:05 +0100

    ion-refresher(https://ionicframework.com/docs/api/refresher)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,6 +80,10 @@ const routes: Routes = [
     path: 'progress',
     loadChildren: () => import('./pages/progress/progress.module').then( m => m.ProgressPageModule)
   },
+  {
+    path: 'refresher',
+    loadChildren: () => import('./pages/refresher/refresher.module').then( m => m.RefresherPageModule)
+  },
   // {
   //   path: 'modal-info',
   //   loadChildren: () => import('./pages/modal-info/modal-info.module').then( m => m.ModalInfoPageModule)
